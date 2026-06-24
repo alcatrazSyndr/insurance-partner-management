c738aaa1373c320a34d9c68790089e56b059eabe
ANTOPC\antos <anto@exordiumgames.com>
2026-06-24 19:03:02 +0200

    initial - visual studio project created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InsurancePartnerManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
